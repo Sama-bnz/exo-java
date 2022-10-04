33f0da062e70acb90f15b2b9adc2d338dd2ace0e
trigueur <thomasbenmezded@gmail.com>
2022-10-04 10:52:36 +0200

    Added name property

--- src/com/openclassroom/Exo.java
+++ src/com/openclassroom/Exo.java
@@ -7,10 +7,17 @@ package com.openclassroom;
 
 public class Exo {
 	/*Le code commence ici*/
+	private String name;
 	
+	public String getName() {
+		return name;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Bonjour tout le monde");
 	}
 	
-
+	public void setName(String name) {
+		this.name= name;
+	}
 }
