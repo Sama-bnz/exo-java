e38c9e2523b559b5aa84ebdf6281112103ff3de1
trigueur <thomasbenmezded@gmail.com>
2022-10-04 10:43:44 +0200

    still learning java with class function

---
+++ src/com/openclassroom/Exo.java
@@ -0,0 +1,16 @@
+package com.openclassroom;
+
+/** Ceci est une implémentation du message traditionnel "Hello world!"
+* @author L'équipe Education d'OpenClassrooms
+*/
+
+
+public class Exo {
+	/*Le code commence ici*/
+	
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde");
+	}
+	
+
+}
