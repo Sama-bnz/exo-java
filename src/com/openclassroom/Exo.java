7aba8fe152bdae72eb336b64aca0697627ac9399
trigueur <thomasbenmezded@gmail.com>
2022-10-04 11:18:02 +0200

    Added age and salary property with getter and setter function

--- src/com/openclassroom/Exo.java
+++ src/com/openclassroom/Exo.java
@@ -7,17 +7,38 @@ package com.openclassroom;
 
 public class Exo {
 	/*Le code commence ici*/
+	
 	private String name;
+	private int age;
+	private int salary;
 	
 	public String getName() {
 		return name;
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Bonjour tout le monde");
 	}
 	
 	public void setName(String name) {
 		this.name= name;
 	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde");
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
 }
